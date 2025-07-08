b550c2f77d79d6faff063ef76935690a82a20d51
Andrew Bednoff <andrew@sentient.foundation>
2025-07-08 23:23:31 +0600

    Made main app config (with DB URL, WS nodes provider URL, and indexing `from_block` and `to_block` cursor settings) in `config.rs` additionally to `contracts,toml` config (contract name, contract address, local ABI JSON path). Implement cursor logic for logs subscription stream, making it replayable according to indexing settings in app configuration file (`from_block` and `to_block`).

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,19 @@ use ethers::types::Address;
 use serde::Deserialize;
 use std::{collections::HashMap, fs, path::Path};
 
+#[derive(Debug, Deserialize)]
+pub struct AppConfig {
+    pub ws_provider: String,
+    pub db_url: String,
+    pub indexing: IndexingConfig,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct IndexingConfig {
+    pub from_block: Option<u64>,
+    pub to_block: Option<u64>,
+}
+
 #[derive(Debug)]
 pub struct ContractConfig {
     pub name: String,
@@ -13,6 +26,12 @@ pub struct ContractConfig {
 struct RawContract {
     address: String,
     abi_path: String,
+}
+
+pub fn load_app_config(path: &str) -> eyre::Result<AppConfig> {
+    let content = fs::read_to_string(Path::new(path))?;
+    let config: AppConfig = toml::from_str(&content)?;
+    Ok(config)
 }
 
 pub fn load_named_contracts(path: &str) -> eyre::Result<Vec<ContractConfig>> {
--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,44 @@ mod db;
 mod event_processor;
 
 use ethers::providers::{Provider, StreamExt, Ws};
-use ethers::types::Filter;
+use ethers::types::{Log, Filter};
 use ethers::middleware::Middleware;
+use ethers::prelude::SubscriptionStream;
 use std::collections::HashMap;
 use tokio;
-use dotenv::dotenv;
-use std::env;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-    dotenv().ok();
-    let db_url = env::var("DATABASE_URL")?;
+    let app_config = config::load_app_config("config.toml")?;
+
+    let db_url = app_config.db_url;
     let pool = db::init_db(&db_url).await;
 
-    let ws = Ws::connect("ws://localhost:8000/stream").await?;
+    let ws = Ws::connect(app_config.ws_provider).await?;
     let provider = Provider::new(ws);
 
-    let configs = config::load_named_contracts("contracts.toml")?;
+    let contracts_config = config::load_named_contracts("contracts.toml")?;
 
     let mut abi_map = HashMap::new();
     let mut address_map = HashMap::new();
 
-    for contract in configs {
+    for contract in contracts_config {
         let abi = abi_loader::load_abi(&contract.abi_path)?;
         abi_map.insert(contract.address, abi);
         address_map.insert(contract.address, contract.name);
     }
 
-    let mut log_stream = provider.subscribe_logs(&Filter::new().select(0u64..)).await?;
-    println!("Listening for logs...");
+    let mut log_stream: SubscriptionStream<_, Log>;
+    let from_block = app_config.indexing.from_block.unwrap_or(0);
+    let to_block = app_config.indexing.to_block;
+
+    if let Some(to_block) = to_block {
+        log_stream = provider.subscribe_logs(&Filter::new().select(from_block..to_block)).await?;
+        println!("Listening for logs from block {} to block {}...", from_block, to_block);
+    } else {
+        log_stream = provider.subscribe_logs(&Filter::new().select(from_block..)).await?;
+        println!("Listening for logs from block {}...", from_block);
+    }
 
     while let Some(log) = log_stream.next().await {
         if let Some(_name) = address_map.get(&log.address) {
