d3e7d2515034ea75af8d7f56b623a821b3f36f25
Andrew Bednoff <andrew@sentient.foundation>
2025-07-21 23:00:07 +0600

    Introduction of NATS as an object storage backend for the Indexer, and as a service bus, where Indexer will be a data provider via pub/sub (producer/consumer) protocol for other SOA backend services.

---
+++ cdk-indexer.v2/src/nats.rs
@@ -0,0 +1,34 @@
+use crate::types::EventPayload;
+use async_nats::jetstream::object_store::{ObjectStore, PutOptions};
+use async_nats::jetstream::JetStream;
+use async_nats::Client;
+
+pub async fn init_nats(nats_url: &str, bucket: &str) -> anyhow::Result<ObjectStore> {
+    let client: Client = async_nats::connect(nats_url).await?;
+    let js = JetStream::new(client);
+    let store = match js.get_object_store(bucket).await {
+        Ok(store) => store,
+        Err(_) => js.create_object_store(async_nats::jetstream::object_store::Config {
+            bucket: bucket.to_string(),
+            ..Default::default()
+        }).await?,
+    };
+    Ok(store)
+}
+
+pub async fn publish_event(
+    store: &ObjectStore,
+    payload: &EventPayload,
+) -> anyhow::Result<()> {
+    let key = format!(
+        "{}:{}:{}:{}",
+        payload.contract_name,
+        payload.event_name,
+        payload.block_number,
+        &payload.transaction_hash
+    );
+
+    let bin = serde_cbor::to_vec(payload)?;
+    store.put(&key, bin.into(), PutOptions::default()).await?;
+    Ok(())
+}
