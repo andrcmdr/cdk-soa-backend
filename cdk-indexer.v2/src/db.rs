538ab2160c44161765c821173f077f9d39d27fb7
Andrew Bednoff <andrew@sentient.foundation>
2025-07-22 23:11:32 +0600

    Made common `EventPayload` type in `types.rs`. Slightly refactor `db.rs` to use `EventPayload` type.

--- cdk-indexer.v2/src/db.rs
+++ cdk-indexer.v2/src/db.rs
@@ -1,37 +1,34 @@
 use std::collections::HashMap;
 use tokio_postgres::Client;
 
+use crate::types::EventPayload;
+
 pub async fn insert_event(
     client: &Client,
-    contract_name: &str,
-    event_name: &str,
-    contract_address: String,
-    tx_hash: &str,
-    block_number: i64,
-    params: HashMap<String, String>,
+    payload: &EventPayload,
 ) -> anyhow::Result<()> {
     let query = r#"
         INSERT INTO contract_events (
             contract_name,
-            event_name,
             contract_address,
-            transaction_hash,
             block_number,
+            transaction_hash,
+            event_name,
             params
         ) VALUES ($1, $2, $3, $4, $5, $6)
     "#;
 
-    let params_json = serde_json::to_value(&params)?;
+    let params_json = serde_json::to_value(&payload.params)?;
 
     client
         .execute(
             query,
             &[
-                &contract_name,
-                &event_name,
-                &contract_address,
-                &tx_hash,
-                &block_number,
+                &payload.contract_name,
+                &payload.contract_address,
+                &payload.block_number,
+                &payload.transaction_hash,
+                &payload.event_name,
                 &params_json,
             ],
         )
---
+++ cdk-indexer.v2/src/types.rs
@@ -0,0 +1,12 @@
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EventPayload {
+    pub contract_name: String,
+    pub contract_address: String,
+    pub block_number: i64,
+    pub transaction_hash: String,
+    pub event_name: String,
+    pub params: HashMap<String, String>,
+}
