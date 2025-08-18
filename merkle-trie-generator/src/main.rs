9ebcf4cfab1bbf35b04ac496ab86dafdbcb2b1e2
Andrew Bednoff <andrew@sentient.foundation>
2025-08-18 22:37:08 +0600

    [MTG] `Main.rs` changes according to contract caller + tx signer in `contract.rs`. Sample `.env` file for app.

--- merkle-trie-generator/src/main.rs
+++ merkle-trie-generator/src/main.rs
@@ -3,22 +3,29 @@ mod trie_builder;
 mod contract;
 mod types;
 
-use alloy_primitives::hex;
+use alloy_primitives::{Address, hex};
 use std::env;
+use std::str::FromStr;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let csv_path = env::args().nth(1).expect("CSV path not provided");
+    let csv_path = env::var("CSV_PATH")?;
+    let private_key = env::var("PRIVATE_KEY")?;
+    let rpc_url = env::var("RPC_URL")?;
+    let contract_address = Address::from_str(&env::var("CONTRACT_ADDRESS")?)?;
+
     let entries = csv_loader::load_airdrop_csv(&csv_path)?;
     let trie_result = trie_builder::build_trie(&entries);
 
     println!("Root Hash: 0x{}", hex::encode(trie_result.root_hash));
 
-    let calldata = contract::encode_contract_call(trie_result.root_hash, trie_result.trie_nodes);
-
-    println!("Encoded Contract Call Data: 0x{}", hex::encode(calldata));
-
-    // ToDo: send transaction using alloy-rpc here
+    contract::send_trie_update(
+        &rpc_url,
+        &private_key,
+        contract_address,
+        trie_result.root_hash,
+        trie_result.trie_nodes,
+    ).await?;
 
     Ok(())
 }
