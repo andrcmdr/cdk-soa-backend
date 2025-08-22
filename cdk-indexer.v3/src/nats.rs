a41c3396a316b6ef2af0345293d384eedbe18068
Andrew Bednoff <andrew@sentient.foundation>
2025-08-22 22:21:56 +0600

    [Indexer] Refactor message bus object data storage (state) layer.

---
+++ cdk-indexer.v3/src/nats.rs
@@ -0,0 +1,66 @@
+use async_nats::{jetstream, jetstream::Context, Client};
+use jetstream::object_store::ObjectStore;
+
+use crate::types::EventPayload;
+
+use std::io::Cursor;
+use std::time::Duration;
+use async_nats::jetstream::object_store::Object;
+use tracing::{error, info};
+
+pub struct Nats {
+    pub client: Client,
+    pub js: Context,
+    pub object_store: ObjectStore,
+}
+
+pub async fn connect(url: &str, bucket: &str) -> anyhow::Result<Nats> {
+    // Create NATS Client with NATS connection, connect to NATS
+    let client = loop {
+        match async_nats::connect(url).await {
+            Ok(conn) => break conn,
+            Err(e) => {
+                error!("[NATS] Connection failed: {}, retrying...", e);
+                tokio::time::sleep(Duration::from_secs(3)).await;
+            }
+        }
+    };
+    info!("[NATS] Connected to NATS at {}", url);
+
+    let js = jetstream::new(client.clone());
+
+    // ensure bucket exists; if already exists, get; otherwise, create
+    let object_store = match js.get_object_store(bucket).await {
+        Ok(store) => store,
+        Err(_) => {
+            info!("[NATS] Creating object store bucket '{}'", bucket);
+            js.create_object_store(jetstream::object_store::Config {
+                bucket: bucket.to_string(),
+                ..Default::default()
+            })
+            .await?
+        },
+    };
+    info!(bucket, "NATS Object Store ready");
+
+    Ok(Nats { client, js, object_store })
+}
+
+pub async fn publish_event(
+    object_store: &ObjectStore,
+    payload: &EventPayload,
+) -> anyhow::Result<()> {
+    let key = format!(
+        "{}::{}::{}::{}::{}",
+        payload.contract_name,
+        payload.contract_address,
+        payload.event_name,
+        payload.event_signature,
+        payload.transaction_hash
+    );
+
+    let bytes = serde_json::to_vec(&serde_json::to_value(payload)?)?;
+    let mut cursor = Cursor::new(bytes);
+    let _obj = object_store.put(key.as_str(), &mut cursor).await?;
+    Ok(())
+}
