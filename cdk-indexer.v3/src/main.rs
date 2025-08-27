9372402a6f9d51000a9bcbe28c53369fe8d73a81
Andrew Bednoff <andrew@sentient.foundation>
2025-08-27 21:26:01 +0600

    [Indexer] Refactor main module. Added project dependencies. Project codebase and libraries stack has been updated to the most recent Alloy 1.0.x versions.

---
+++ cdk-indexer.v3/src/main.rs
@@ -0,0 +1,35 @@
+mod config;
+mod db;
+mod nats;
+mod abi;
+mod subscriptions;
+mod event_decoder;
+mod types;
+
+use crate::subscriptions::EventProcessor;
+
+use std::path::Path;
+use tracing_subscriber::{EnvFilter, fmt};
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // logging
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    fmt().with_env_filter(filter).compact().init();
+
+    let cfg_path = std::env::args().nth(1).unwrap_or_else(|| "./config.yaml".to_string());
+    let cfg = config::AppCfg::load(&cfg_path)?;
+
+    let db_schema_path = std::env::args().nth(2).unwrap_or_else(|| "./init.sql".to_string());
+    let db_schema = std::fs::read_to_string(Path::new(&db_schema_path))?;
+
+    // deps
+    let pg = db::connect_pg(&cfg.postgres.dsn, db_schema.as_str()).await?;
+    let nats = nats::connect(&cfg.nats.url, &cfg.nats.object_store_bucket).await?;
+
+    let event_processor = EventProcessor::new(&cfg, pg, nats).await?;
+    event_processor.run().await?;
+
+    Ok(())
+}
